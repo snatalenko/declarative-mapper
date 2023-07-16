c1c4b5501a215502875e4d3c933dc5d759ea76f7
Stanislav Natalenko <stas@natalenko.com>
2023-07-16 13:39:07 +0100

    Fix: Files placement in Dist folder

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@
 export { default as createMapper } from './createMapper';
 export { default as mappingForSchema } from './mappingForSchema';
 export { default as sampleForSchema } from './sampleForSchema';
+export * from './TMapping';
+
+// @ts-ignore
 import * as schema from '../schemas/mapping.json';
 export { schema as mappingSchema };
-export * from './TMapping';
